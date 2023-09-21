aa0bf70623bec4fae8d69868d7fb21ad9d66d84c
Boris Okner <boris.okner@gmail.com>
2023-09-21 12:40:17 -0400

    Use Croma expansion

---
+++ lib/croma_expand.ex
@@ -0,0 +1,63 @@
+defmodule CromaExpand do
+  use Croma
+  alias Sourceror.Zipper, as: Z
+
+  require Logger
+
+  def expand(source) do
+    Sourceror.Zipper.Inspect.default_inspect_as(:as_code)
+
+    zipper =
+      source
+      |> Code.string_to_quoted!()
+      |> Macro.prewalk(fn
+        {defun, _, _} = defun_ast when defun in [:defun, :defunp] ->
+          Macro.expand(defun_ast, __ENV__)
+
+        other ->
+          other
+      end)
+      |> Z.zip()
+
+    {new_zipper, _} =
+      Z.traverse(zipper, nil, fn
+        %Z{node: {:__block__, _, block_children}} = block, acc ->
+          block_source = Z.node(block) |> Sourceror.to_string()
+
+          if String.starts_with?(block_source, "@spec ") do
+            Logger.debug("Found enclosed spec!")
+
+            {Enum.reduce(flatten_block_children(block_children), block, fn def_child, block_acc ->
+               Z.insert_left(block_acc, def_child)
+             end)
+             |> Z.remove(), acc}
+          else
+            {block, acc}
+          end
+
+        other, acc ->
+          {other, acc}
+      end)
+
+    new_zipper
+  end
+
+  def flatten_block_children(children) do
+    Enum.reduce(children, [], fn
+      {:__block__, _, sub_children}, acc ->
+        acc ++ sub_children
+
+      flat, acc ->
+        acc ++ [flat]
+    end)
+  end
+
+  def insert_def(block_zipper, {:__block__, _, children} = _block) do
+    Logger.debug("Block within spec")
+    # Z.insert_left(zipper, block)
+    Enum.reduce(children, block_zipper, fn def_child, block_acc ->
+      Z.insert_left(block_acc, def_child)
+    end)
+    |> Z.remove()
+  end
+end
